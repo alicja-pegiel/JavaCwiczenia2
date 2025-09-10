4e81a92a995692ee8f1d94cb1bc700ce198822bd
Alicja Gorgoń <alicja.pegiel@zskocjan.pl>
2025-09-10 12:28:33 +0200

    Proste typy danych

--- PodstawyJavy/src/Hello.java
+++ PodstawyJavy/src/Hello.java
@@ -1,5 +1,18 @@
 public class Hello {
     public static void main(String[] args) {
+        // To jest komentarz
         System.out.println("Hello World");
+        // Komentowanie wielu linii - ctrl + /
+        /*
+        Tu mogę pisać
+        I każda nowa linijka
+        Też jest w komentarzu
+         */
+        int a = 5, b = 2;
+        System.out.println("Wynik dodawania: " + (a + b));
+        System.out.println(a + b + " to wynik dodawania");
+        float wynik;
+        wynik = (float) a/b;
+        System.out.println(wynik);
     }
 }
